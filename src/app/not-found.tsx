67a647ba2ab63d9fa4f75576e3c261a138949c2f
Apinan Loratsachan <sunseasame2556@gmail.com>
2024-12-30 08:10:41 +0700

    v0.2.2

---
+++ src/app/not-found.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { Card, CardBody, CardHeader, Divider } from "@nextui-org/react";
+import { useTheme } from "next-themes";
+
+export default function NotFound() {
+  const { setTheme, theme } = useTheme();
+  const isDarkMode = typeof window !== "undefined" ? theme === "dark" : false;
+  return (
+    <div className="flex items-center justify-center">
+      <Card
+        className="card m-auto"
+        isBlurred
+        shadow="lg"
+        style={{
+          overflow: "hidden",
+          backgroundColor: isDarkMode
+            ? "hsla(0,0%,0%,.8)"
+            : "hsla(0,0%,100%,.8)",
+          height: "100%",
+          width: "100%",
+        }}
+      >
+        <CardHeader className="justify-center">
+          <div className="text-center">
+            <b style={{ fontSize: "50px" }}>404</b>
+            <p>
+              <b>Page Not Found</b>
+            </p>
+          </div>
+        </CardHeader>
+        <Divider />
+        <CardBody className="text-center justify-center items-center">
+          The page you are looking for does not exist.
+        </CardBody>
+      </Card>
+    </div>
+  );
+}
